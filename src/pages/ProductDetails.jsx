a82be463e35f97e5423e733e856ff1887669678f
mdsujan-mridha <mdsujan134523@gmail.com>
2024-09-14 17:58:34 +0600

    update product details

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import { Rating } from '@mui/material';
 import React, { Fragment, useEffect, useState } from 'react';
+import Carousel from 'react-material-ui-carousel';
 import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
@@ -25,18 +26,21 @@ const ProductDetails = () => {
     
     return (
         <Fragment>
-          <div className='min-h-screen px-12 w-full'>
+          <div className='min-h-screen px-12 w-full pt-24'>
             <div className="flex h-96 justify-between items-center gap-5">
-            <div className='w-1/2'></div>
+            <div style={{width:"600px",height:"600px",objectFit:"contain",marginTop:"50px"}} className='border-2'>
+             <Carousel style={{width:"600px",height:"600px",objectFit:"cover"}}>
+                <img src={product?.image} alt={product?.title} style={{width:"600px",height:"600px",objectFit:"contain"}}/>
+             </Carousel>
+            </div>
                 <div className='w-1/2 flex justify-start flex-col gap-5'>
                     <h1 className='lato-bold text-4xl'> {product?.title} </h1>
                     <div className='flex justify-start gap-5 items-center'>
-                    <Rating {...options}/> <span className=' font-bold'> {product?.rating.count} Reviews </span>
+                    <Rating {...options}/> <span className=' font-bold'> {product?.rating?.count} Reviews </span>
                     </div>
                     <p className='text-justify'>{product?.description}</p>
                 </div>
             </div>
-                
           </div>
         </Fragment>
     );
